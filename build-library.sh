24f6d105c3889681cec8901ebe12ecb55e24ee68
Michael L Heuer <heuermh@acm.org>
2019-10-24 10:50:17 -0500

    update processing-core dependency version to 3.5.3

--- build-library.sh
+++ build-library.sh
@@ -10,6 +10,6 @@ cp lib/guava-28.1-jre.jar executor/library
 cp -R src executor
 cp -R examples executor
 cd src
-javac -source 1.8 -target 1.8 -classpath "../lib/processing-core-3.3.6.jar:../lib/guava-28.1-jre.jar" org/dishevelled/processing/executor/*.java
+javac -source 1.8 -target 1.8 -classpath "../lib/processing-core-3.5.3.jar:../lib/guava-28.1-jre.jar" org/dishevelled/processing/executor/*.java
 jar cvf ../executor/library/executor.jar org/dishevelled/processing/executor/*.class
 cd ..
