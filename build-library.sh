34fd8be29da27ea7b4bc05296acbb9eb759f016e
Michael L Heuer <heuermh@acm.org>
2019-10-24 10:18:22 -0500

    update guava to version 28.1-jre, processing-core to 3.3.6

--- build-library.sh
+++ build-library.sh
@@ -6,10 +6,10 @@ cp COPYING executor
 cp README executor
 cp library.properties executor
 cp lib/guava.COPYING executor/guava.COPYING
-cp lib/guava-19.0.jar executor/library
+cp lib/guava-28.1-jre.jar executor/library
 cp -R src executor
 cp -R examples executor
 cd src
-javac -source 1.6 -target 1.6 -classpath "../lib/processing-core-2.2.1.jar:../lib/guava-19.0.jar" org/dishevelled/processing/executor/*.java
+javac -source 1.8 -target 1.8 -classpath "../lib/processing-core-3.3.6.jar:../lib/guava-28.1-jre.jar" org/dishevelled/processing/executor/*.java
 jar cvf ../executor/library/executor.jar org/dishevelled/processing/executor/*.class
 cd ..
